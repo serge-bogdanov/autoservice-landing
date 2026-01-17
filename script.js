dc14c2da47b63292b91e6c1266e5bcfa51d2faef
serge-bogdanov <si000bi01@gmail.com>
2026-01-17 22:01:23 +0500

    feat(achievement): add achievement banner and carousel

--- script.js
+++ script.js
@@ -24,13 +24,34 @@ if (document.documentElement.offsetWidth <= 1180) {
   menuPhoneNumber.setAttribute("href", "mailto:+79279971242")
   menuPhoneNumber.classList.add("menu__number")
   menu.append(menuPhoneNumber)
-  
+
   const menuCopyright = document.querySelector(".footer__copyright").cloneNode(true)
   menuCopyright.classList.add("menu__copyright")
   const menuSocials = document.querySelector(".footer__contact-value-socials").cloneNode(true)
   menuSocials.classList.add("menu__socials")
   menu.append(menuSocials)
-  menu.append(menuCopyright)  
+  menu.append(menuCopyright)
 
 }
 
+const prevButton = document.querySelector("[data-js-prev-button]")
+const nextButton = document.querySelector("[data-js-next-button]")
+const track = document.querySelector("[data-js-track]")
+
+const getStep = () => {
+  const slide = track.firstElementChild;
+  if (!slide) {
+    return 0;
+  }
+  const slideWidth = slide.getBoundingClientRect().width;
+  const slideGap = parseInt(getComputedStyle(track).getPropertyValue("--slides-gap"));
+  return slideWidth + slideGap;
+}
+
+
+prevButton.addEventListener("click", () => {
+  track.scrollBy({ left: -getStep(), behavior: "smooth" })
+})
+nextButton.addEventListener("click", () => {
+  track.scrollBy({ left: getStep(), behavior: "smooth" })
+})
